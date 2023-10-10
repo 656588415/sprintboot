685dd00ea0468f99fc2956152782f632de84ab7c
656588415 <omolokostella@icloud.com>
2023-10-10 11:32:57 +0100

    Premier code spting boot avec petite ereure

---
+++ src/main/java/stella/dev/etudiant/EtudiantApplication.java
@@ -0,0 +1,21 @@
+package stella.dev.etudiant;
+
+import java.lang.*;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtudiantApplication implements CommandLineRunner {
+	System.out.println("Hellow world");
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtudiantApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
